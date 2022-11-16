aafaa0e01f516e286409464470df793fe68423c5
Alexander Bentkamp <bentkamp@gmail.com>
2022-11-16 11:47:12 +0100

    fix ecosystem file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 // This is a configuration file for pm2, a production process manager for nodejs
 module.exports = {
   apps : [{
-    name   : "lean4web",
+    name   : "lean4game",
     script : "server/index.js",
     env: {
        NODE_ENV: "production",
