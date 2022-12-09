d55730ee7ea5360272a25a651e8ff3adc6409fa2
Jon <jon.eugster@gmx.ch>
2022-12-09 16:55:09 +0100

    update mathlib to master

--- server/testgame/lakefile.lean
+++ server/testgame/lakefile.lean
@@ -4,7 +4,7 @@ open Lake DSL
 require GameServer from ".."/"leanserver"
 
 require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git"@"9b15aa6f091a623f992d6fff76167864794ce350"
+  "https://github.com/leanprover-community/mathlib4.git"@"master"
 
 --set_option tactic.hygienic false
 --set_option autoImplicit false
