f98ff9fa109462cf6ffcdbbfbfc5509ea18671f0
Alexander Bentkamp <bentkamp@gmail.com>
2023-01-19 17:00:16 +0100

    lake exe cache get

--- server/build.sh
+++ server/build.sh
@@ -6,6 +6,6 @@ cd $(dirname $0)
 # Build elan image if not already present
 docker build --pull --rm -f elan.Dockerfile -t elan:latest .
 
-(cd testgame && lake build)
+(cd testgame && lake exe cache get && lake build)
 docker rmi testgame:latest || true
 docker build --rm -f server.Dockerfile -t testgame:latest .
