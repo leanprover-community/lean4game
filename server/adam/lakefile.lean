b6715bcbffba02376cc3db2a47e6f631f81a7090
Jon Eugster <eugster.jon@gmail.com>
2023-04-20 15:45:06 +0200

    add global options to the games (disbable tactic.hygienic)

--- server/adam/lakefile.lean
+++ server/adam/lakefile.lean
@@ -6,9 +6,9 @@ require GameServer from ".."/"leanserver"
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"@"master"
 
-package Adam
+package Adam where
+  moreLeanArgs := #["-DautoImplicit=false", "-Dtactic.hygienic=false"]
+  moreServerArgs := #["-DautoImplicit=false", "-Dtactic.hygienic=false"]
 
 @[default_target]
-lean_lib Adam {
-  moreLeanArgs := #["-DautoImplicit=false"]
-}
+lean_lib Adam
--- server/nng/lakefile.lean
+++ server/nng/lakefile.lean
@@ -3,12 +3,12 @@ open Lake DSL
 
 require GameServer from ".."/"leanserver"
 
-
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git" @ "fc4a489c2af75f687338fe85c8901335360f8541"
-package NNG
+
+package NNG where
+  moreLeanArgs := #["-DautoImplicit=false", "-Dtactic.hygienic=false"]
+  moreServerArgs := #["-DautoImplicit=false", "-Dtactic.hygienic=false"]
 
 @[default_target]
-lean_lib NNG {
-  moreLeanArgs := #["-DautoImplicit=false"]
-}
+lean_lib NNG
