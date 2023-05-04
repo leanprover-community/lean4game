b703752f3af7fc895684b687d739f873144f489d
Jon Eugster <eugster.jon@gmail.com>
2023-05-04 19:30:53 +0200

    css and typo

--- client/src/components/LandingPage.tsx
+++ client/src/components/LandingPage.tsx
@@ -26,8 +26,7 @@ function GithubIcon({url='https://github.com'}) {
 
   return <div className="github-link">
     <a title="view the Lean game server on Github" href={url}>
-    <svg height="24
-    " aria-hidden="true" viewBox="0 0 16 16" version="1.1" width="24" className="">
+    <svg height="24" aria-hidden="true" viewBox="0 0 16 16" version="1.1" width="24" className="">
       {/* <circle className="world-circle" cx="8" cy="8" r="8" fill="#fff"/> */}
       <path fill="#fff" d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z"></path>
     </svg>
@@ -99,9 +98,9 @@ function LandingPage() {
       <div id="main-title">
         <h1>Lean Game Server</h1>
         <p>
-          This server hosts learning games for the
-          proof assistant <a target="_blank" href="https://leanprover-community.github.io/">Lean</a> <i>(lean4)</i> and
-          its mathematical library <a target="_blank" href="https://github.com/leanprover-community/mathlib4">Mathlib</a>.
+          A repository of learning games for the
+          proof assistant <a target="_blank" href="https://leanprover-community.github.io/">Lean</a> <i>(Lean 4)</i> and
+          its mathematical library <a target="_blank" href="https://github.com/leanprover-community/mathlib4">mathlib</a>
         </p>
       </div>
     </header>
@@ -137,12 +136,12 @@ This is a good first introduction to Lean!"
       <div className="wrapper">
         <h2>Development notes</h2>
         <p>
-          As this server runs lean on our university machines, it has a capacity of ca. 15-25
-          games running simultaneously. We hope to address this limitation in the further future.
+          As this server runs lean on our university machines, it has a limited capacity of 15-25
+          games running simultaneously. We hope to address this limitation in the future.
         </p>
         <p>
-          Most things about the games and infrastructure is still in development. Feel free to
-          write <a target="_blank" href="https://github.com/leanprover-community/lean4game/issues">Github Issues</a> about
+          Most aspects of the games and the infrastructure are still in development. Feel free to
+          file a <a target="_blank" href="https://github.com/leanprover-community/lean4game/issues">GitHub Issue</a> about
           any problems you experience!
         </p>
       </div>
@@ -151,7 +150,7 @@ This is a good first introduction to Lean!"
       <div className="wrapper">
         <h2>Adding new games</h2>
         <p>
-          If you consider writing your own game, you should use
+          If you are considering writing your own game, you should use
           the <a target="_blank" href="https://github.com/hhu-adam/NNG4">NNG Github Repo</a> as
           a template.
         </p>
@@ -169,7 +168,7 @@ This is a good first introduction to Lean!"
       <div className="wrapper">
         <h2>Funding</h2>
         <p>
-          This Server has been developed as part of the
+          This server has been developed as part of the
           project <a target="_blank" href="https://hhu-adam.github.io">ADAM : Anticipating the Digital Age of Mathematics</a> at
           Heinrich-Heine-Universität in Düsseldorf.
         </p>
