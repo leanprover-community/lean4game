00f86ab79d540c99653ce1ca321cf92679275172
Ahmed Sami <ahmedsami03@protonmail.com>
2025-02-26 19:13:49 +0100

    Remove leftover comment

--- server/GameServer/FileWorker.lean
+++ server/GameServer/FileWorker.lean
@@ -126,7 +126,6 @@ partial def findForbiddenTactics (inputCtx : Parser.InputContext) (workerState :
     let allowed := GameServer.ALLOWED_KEYWORDS
     -- Ignore syntax elements that do not start with a letter or are listed above.
     if 0 < val.length ∧ val.data[0]!.isAlpha ∧ not (allowed.contains val) then
-      -- Treat `simp?` and `simp!` like `simp`
       match levelInfo.tactics.find? (·.name.toString == val) with
       | none =>
         -- Tactic will never be introduced in the game.
