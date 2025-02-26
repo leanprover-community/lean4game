0173688b21e0516ad3549a801709ee46d96fc1f7
Jon Eugster <eugster.jon@gmail.com>
2025-02-26 11:55:59 +0100

    align relay with file from lean4web

--- relay/index.mjs
+++ relay/index.mjs
@@ -24,20 +24,78 @@ const queueLength = {
   "g/trequetrum/lean4game-logic": 2,
 }
 
+/** We keep queues of started Lean Server processes to be ready when a user arrives */
+let queue = {}
+
+let socketCounter = 0
+
+function logStats() {
+  console.log(`[${new Date()}] Number of open sockets - ${socketCounter}`)
+  console.log(`[${new Date()}] Free RAM - ${Math.round(os.freemem() / 1024 / 1024)} / ${Math.round(os.totalmem() / 1024 / 1024)} MB`)
+}
+
+function getTag(owner, repo) {
+  return `g/${owner.toLowerCase()}/${repo.toLowerCase()}`
+}
+
+function getGameDir(owner, repo) {
+  owner = owner.toLowerCase()
+  if (owner == 'local' || owner == 'test' ) {
+    if(!isDevelopment) {
+      console.error(`No local games in production mode.`)
+      return ""
+    }
+  } else {
+    if(!fs.existsSync(path.join(__dirname, '..', 'games'))) {
+      console.error(`Did not find the following folder: ${path.join(__dirname, '..', 'games')}`)
+      console.error('Did you already import any games?')
+      return ""
+    }
+  }
+
+  let gameDir =
+    // note: in the local case we need `repo` to be case sensitive
+    (owner == 'local') ? path.join(__dirname, '..', '..', repo) :
+    (owner == 'test') ? path.join(__dirname, '..', 'server') :
+      path.join(__dirname, '..', 'games', `${owner}`, `${repo.toLowerCase()}`)
+
+  if(!fs.existsSync(gameDir)) {
+    console.error(`Game '${gameDir}' does not exist!`)
+    return ""
+  }
+
+  return gameDir
+}
+
+/** start Lean Server processes to refill the queue */
+function fillQueue(tag) {
+  while (queue[tag].length < queueLength[tag]) {
+    let serverProcess
+    serverProcess = startServerProcess(tag)
+    if (serverProcess == null) {
+      console.error('serverProcess was undefined/null')
+      return
+    }
+    queue[tag].push(serverProcess)
+  }
+}
+
 const __filename = url.fileURLToPath(import.meta.url)
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url))
 
+const environment = process.env.NODE_ENV
+const isDevelopment = environment === 'development'
+
+const crtFile = process.env.SSL_CRT_FILE
+const keyFile = process.env.SSL_KEY_FILE
+
 const app = express()
 
-const PORT = process.env.PORT || 8080
-
 var router = express.Router()
-
 router.get('/import/status/:owner/:repo', importStatus)
 router.get('/import/trigger/:owner/:repo', importTrigger)
 
-const server = app
-  .use(express.static(path.join(__dirname, '..', 'client', 'dist'))) // TODO: add a dist folder from inside the game
+app.use(express.static(path.join(__dirname, '..', 'client', 'dist'))) // TODO: add a dist folder from inside the game
   .use('/i18n/g/:owner/:repo/:lang/*', (req, res, next) => {
     const owner = req.params.owner
     const repo = req.params.repo
@@ -111,105 +169,69 @@ const server = app
     })
   })
   .use('/', router)
-  .listen(PORT, () => console.log(`Listening on ${PORT}`))
+
+let server
+if (crtFile && keyFile) {
+  var privateKey  = fs.readFileSync(keyFile, 'utf8');
+  var certificate = fs.readFileSync(crtFile, 'utf8');
+  var credentials = {key: privateKey, cert: certificate};
+
+  const PORT = process.env.PORT ?? 443
+  server = https.createServer(credentials, app).listen(PORT,
+    () => console.log(`HTTPS on port ${PORT}`));
+
+  // redirect http to https
+  express().get('*', function(req, res) {
+    res.redirect('https://' + req.headers.host + req.url).listen(80);
+  })
+} else {
+  const PORT = process.env.PORT ?? 8080
+  server = app.listen(PORT,
+    () => console.log(`HTTP on port ${PORT}`))
+}
 
 const wss = new WebSocketServer({ server })
 
-var socketCounter = 0
-
-const environment = process.env.NODE_ENV
-const isDevelopment = environment === 'development'
-
-/** We keep queues of started Lean Server processes to be ready when a user arrives */
-const queue = {}
-
-function getTag(owner, repo) {
-  return `g/${owner.toLowerCase()}/${repo.toLowerCase()}`
-}
-
-function getGameDir(owner, repo) {
-  owner = owner.toLowerCase()
-  if (owner == 'local' || owner == 'test' ) {
-    if(!isDevelopment) {
-      console.error(`No local games in production mode.`)
-      return ""
-    }
-  } else {
-    if(!fs.existsSync(path.join(__dirname, '..', 'games'))) {
-      console.error(`Did not find the following folder: ${path.join(__dirname, '..', 'games')}`)
-      console.error('Did you already import any games?')
-      return ""
-    }
-  }
-
-  let game_dir =
-    // note: in the local case we need `repo` to be case sensitive
-    (owner == 'local') ? path.join(__dirname, '..', '..', repo) :
-    (owner == 'test') ? path.join(__dirname, '..', 'server') :
-      path.join(__dirname, '..', 'games', `${owner}`, `${repo.toLowerCase()}`)
-
-  if(!fs.existsSync(game_dir)) {
-    console.error(`Game '${game_dir}' does not exist!`)
-    return ""
-  }
-
-  return game_dir
-}
-
 function startServerProcess(owner, repo) {
-  let game_dir = getGameDir(owner, repo)
-  if (!game_dir) { return }
+  let gameDir = getGameDir(owner, repo)
+  if (!gameDir) { return }
 
   let serverProcess
   if (isDevelopment) {
     console.warn("Running without Bubblewrap container!")
+    serverProcess = cp.spawn("lean", ["--server"], { cwd: gameDir })
 
-    let args = ["--server", game_dir]
-    let binDir = path.join(game_dir, ".lake", "packages", "GameServer", "server", ".lake", "build", "bin")
-    // Note: `cwd` is important to be the `bin` directory as `Watchdog` calls `./gameserver` again
-    if (fs.existsSync(binDir)) {
-      // Try to use the game's own copy of `gameserver`.
-      serverProcess = cp.spawn("./gameserver", args, { cwd: binDir })
-    } else {
-      // If the game is built with `-Klean4game.local` there is no copy in the lake packages.
-      serverProcess = cp.spawn("./gameserver", args,
-        { cwd: path.join(__dirname, "..", "server", ".lake", "build", "bin") })
-    }
+    // let args = ["--server", gameDir]
+    // let binDir = path.join(gameDir, ".lake", "packages", "GameServer", "server", ".lake", "build", "bin")
+    // // Note: `cwd` is important to be the `bin` directory as `Watchdog` calls `./gameserver` again
+    // if (fs.existsSync(binDir)) {
+    //   // Try to use the game's own copy of `gameserver`.
+    //   serverProcess = cp.spawn("./gameserver", args, { cwd: binDir })
+    // } else {
+    //   // If the game is built with `-Klean4game.local` there is no copy in the lake packages.
+    //   serverProcess = cp.spawn("./gameserver", args,
+    //     { cwd: path.join(__dirname, "..", "server", ".lake", "build", "bin") })
+    // }
   } else {
     serverProcess =  cp.spawn("./bubblewrap.sh",
-      [ game_dir, path.join(__dirname, '..')],
+      [ gameDir, path.join(__dirname, '..')],
       { cwd: __dirname }
     )
   }
 
+  serverProcess.stderr?.on('data', data =>
+    console.error(`Lean Server: ${data}`)
+  )
+
   serverProcess.on('error', error =>
     console.error(`Launching Lean Server failed: ${error}`)
   )
-
-  if (serverProcess.stderr !== null) {
-    serverProcess.stderr.on('data', data =>
-      console.error(`Lean Server: ${data}`)
-    )
-  }
 
   serverProcess.on('close', (code, _signal) => {
     console.log(`Lean server exited with code ${code}`)
   })
 
   return serverProcess
-}
-
-/** start Lean Server processes to refill the queue */
-function fillQueue(tag) {
-  while (queue[tag].length < queueLength[tag]) {
-    let serverProcess
-    serverProcess = startServerProcess(tag)
-    if (serverProcess == null) {
-      console.error('serverProcess was undefined/null')
-      return
-    }
-    queue[tag].push(serverProcess)
-  }
 }
 
 // // TODO: We disabled queue for now
@@ -223,63 +245,57 @@ function fillQueue(tag) {
 const urlRegEx = /^\/websocket\/g\/([\w.-]+)\/([\w.-]+)$/
 
 wss.addListener("connection", function(ws, req) {
-    const reRes = urlRegEx.exec(req.url)
-    if (!reRes) {
-      console.error(`Connection refused because of invalid URL: ${req.url}`)
-      return
-    }
-    const owner = reRes[1]
-    const repo = reRes[2]
+  const reRes = urlRegEx.exec(req.url)
+  if (!reRes) {
+    console.error(`Connection refused because of invalid URL: ${req.url}`)
+    return
+  }
+  const owner = reRes[1]
+  const repo = reRes[2]
+  const tag = getTag(owner, repo)
+  const ip = anonymize(req.headers['x-forwarded-for'] || req.socket.remoteAddress)
+  let ps
+  if (!queue[tag] || queue[tag].length == 0) {
+    ps = startServerProcess(owner, repo)
+  } else {
+    console.info('Got process from the queue')
+    ps = queue[tag].shift() // Pick the first Lean process; it's likely to be ready immediately
+    fillQueue(tag)
+  }
 
-    const tag = getTag(owner, repo)
+  if (ps == null) {
+    console.error('server process is undefined/null')
+    return
+  }
 
-    let ps
-    if (!queue[tag] || queue[tag].length == 0) {
-      ps = startServerProcess(owner, repo)
-    } else {
-        console.info('Got process from the queue')
-        ps = queue[tag].shift() // Pick the first Lean process; it's likely to be ready immediately
-        fillQueue(tag)
-    }
+  const socket = {
+    onMessage: (cb) => { ws.on("message", cb) },
+    onError: (cb) => { ws.on("error", cb) },
+    onClose: (cb) => { ws.on("close", cb) },
+    send: (data, cb) => { ws.send(data,cb) }
+  }
+  const reader = new rpc.WebSocketMessageReader(socket)
+  const writer = new rpc.WebSocketMessageWriter(socket)
+  const socketConnection = jsonrpcserver.createConnection(reader, writer, () => ws.close())
+  const serverConnection = jsonrpcserver.createProcessStreamConnection(ps)
+  socketConnection.forward(serverConnection, message => {
+    if (isDevelopment) {console.log(`CLIENT: ${JSON.stringify(message)}`)}
+    return message
+  })
+  serverConnection.forward(socketConnection, message => {
+    if (isDevelopment) {console.log(`SERVER: ${JSON.stringify(message)}`)}
+    return message
+  })
 
-    if (ps == null) {
-      console.error('server process is undefined/null')
-      return
-    }
+  ws.on('close', () => {
+    console.log(`[${new Date()}] Socket closed - ${ip}`)
+    socketCounter -= 1
+    logStats()
+  })
 
-    socketCounter += 1
-    const ip = anonymize(req.headers['x-forwarded-for'] || req.socket.remoteAddress)
+  socketConnection.onClose(() => serverConnection.dispose())
+  serverConnection.onClose(() => socketConnection.dispose())
 
-    // TODO (Matvey): extract further information from `req`, for example browser language.
-    console.log(`[${new Date()}] Socket opened - ${ip} - ${owner}/${repo}`)
-
-    const socket = {
-        onMessage: (cb) => { ws.on("message", cb) },
-        onError: (cb) => { ws.on("error", cb) },
-        onClose: (cb) => { ws.on("close", cb) },
-        send: (data, cb) => { ws.send(data,cb) }
-    }
-    const reader = new rpc.WebSocketMessageReader(socket)
-    const writer = new rpc.WebSocketMessageWriter(socket)
-    const socketConnection = jsonrpcserver.createConnection(reader, writer, () => ws.close())
-    const serverConnection = jsonrpcserver.createProcessStreamConnection(ps)
-    socketConnection.forward(serverConnection, message => {
-        if (isDevelopment) {console.log(`CLIENT: ${JSON.stringify(message)}`)}
-        return message
-    })
-    serverConnection.forward(socketConnection, message => {
-      if (isDevelopment) {console.log(`SERVER: ${JSON.stringify(message)}`)}
-        return message
-    })
-
-    console.log(`[${new Date()}] Number of open sockets - ${socketCounter}`)
-    console.log(`[${new Date()}] Free RAM - ${Math.round(os.freemem() / 1024 / 1024)} / ${Math.round(os.totalmem() / 1024 / 1024)} MB`)
-
-    ws.on('close', () => {
-      console.log(`[${new Date()}] Socket closed - ${ip}`)
-      socketCounter -= 1
-    })
-
-    socketConnection.onClose(() => serverConnection.dispose())
-    serverConnection.onClose(() => socketConnection.dispose())
+  socketCounter += 1
+  logStats()
 })
