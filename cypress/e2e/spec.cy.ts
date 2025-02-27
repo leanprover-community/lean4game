f721de7b1845edb5c5a3b4282ce3bb23f6a19416
Ahmed Sami <ahmedsami03@protonmail.com>
2025-02-27 19:39:18 +0100

    Add more tests

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,7 +1,42 @@
-describe('Test Game', () => {
-  it('displays the world overview', () => {
+describe('The world selection', () => {
+  it('displays the game title', () => {
     cy.visit('http://localhost:3000/#/g/test/Test')
-    cy.contains('p.world-title', 'Demo World 1').should('exist');
-    cy.contains('p.world-title', 'Demo World 2').should('exist');
+    cy.contains('span.nav-title', 'Test Game').should('exist')
+  })
+
+  it('displays the rules popup', () => {
+    cy.visit('http://localhost:3000/#/g/test/Test')
+    cy.contains('a.nav-button', 'Rules').click()
+    cy.contains('div.modal', 'Game Rules').should('exist')
+  })
+
+  it('displays the world titles', () => {
+    cy.visit('http://localhost:3000/#/g/test/Test')
+    cy.get('p.world-title').should(($p) => {
+      const titles = $p.map((_, el) => el.textContent).get()
+      expect(titles).to.have.members(['Demo World 1', 'Demo World 2'])
+    })
+  })
+
+  it('displays the first world and its first level as selectable', () => {
+    cy.visit('http://localhost:3000/#/g/test/Test')
+    const world = cy.contains('svg.world-selection>a:not(.disabled)', 'Demo World 1')
+    const assertOn = [world, world.next('a.level')]
+
+    assertOn.forEach((value) => value
+        .should('have.attr', 'href', '#/g/test/Test/world/DemoWorld1/level/0')
+        .and('have.css', 'cursor', 'pointer')
+    )
+  })
+
+  it('displays the second world and its first level as disabled', () => {
+    cy.visit('http://localhost:3000/#/g/test/Test')
+    const world = cy.contains('svg.world-selection>a.disabled', 'Demo World 2')
+    const assertOn = [world, world.next('a.level.disabled')]
+
+    assertOn.forEach((value) => value
+        .should('have.attr', 'href', '#/g/test/Test')
+        .and('have.css', 'cursor', 'default')
+    )
   })
 })
