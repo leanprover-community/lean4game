4335b86195a72c3db340fdd93ce3f271add9b82b
Ahmed Sami <ahmedsami03@protonmail.com>
2025-02-28 19:56:22 +0100

    Ignore uncaught exceptions

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -57,3 +57,9 @@ Cypress.Commands.add('map', { prevSubject: true }, (subject: unknown[], iteratee
         { log: false }
     );
 });
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+    // returning false here prevents Cypress from
+    // failing the test
+    return false;
+});
