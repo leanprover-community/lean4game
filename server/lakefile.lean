c725eb846801ab8c50580fcb3ee6a4d994ddabd0
Alexander Bentkamp <bentkamp@gmail.com>
2025-07-13 21:34:46 +0200

    remove GameServerExe from lakefile

--- server/lakefile.lean
+++ server/lakefile.lean
@@ -11,21 +11,5 @@ require i18n from git "https://github.com/hhu-adam/lean-i18n.git" @ leanVersion
 
 require importGraph from git "https://github.com/leanprover-community/import-graph" @ leanVersion
 
-
-lean_lib GameServer
-
 @[default_target]
-lean_exe gameserver {
-  root := `GameServer
-  supportInterpreter := true
-}
-
-/--
-When a package depending on GameServer updates its dependencies,
-build the `gameserver` executable.
--/
-post_update pkg do
-  let rootPkg ← getRootPackage
-  if rootPkg.name = pkg.name then
-    return -- do not run in GameServer itself
-  discard <| runBuild gameserver.build >>= (·.await)
+lean_lib GameServer
