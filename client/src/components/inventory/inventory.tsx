3391e8cedac6b3c7b72155c99167e716376b3c7c
Jon Eugster <eugster.jon@gmail.com>
2025-09-13 18:23:25 +0200

    add file

---
+++ client/src/components/inventory/inventory.tsx
@@ -0,0 +1,51 @@
+import React, { useEffect } from "react"
+import { useTranslation } from "react-i18next"
+import { InventoryOverview, InventoryTile, LevelInfo } from "../../state/api"
+// import { InventoryList } from "./InventoryList"
+import { InventoryTab, inventoryTabAtom, inventoryTilesAtoms, selectedDocTileAtom } from "../../store/inventory-atoms"
+import { useAtom } from "jotai"
+import { InventorySubTabBar, InventoryTabBar } from "./tab_bars"
+import { InventoryList } from "./inventory_list"
+
+
+function TabContent(type: InventoryTab, levelInfo: LevelInfo | InventoryOverview | undefined): InventoryTile[] {
+  switch(type) {
+    case InventoryTab.theorem: return levelInfo?.lemmas ?? []
+    case InventoryTab.tactic: return levelInfo?.tactics ?? []
+    case InventoryTab.definition: return levelInfo?.definitions ?? []
+    default:
+      const _exhaustive: never = type
+  }
+}
+
+export function Inventory({levelInfo, enableAll=false} :
+  {
+    levelInfo: LevelInfo | InventoryOverview | undefined,
+    enableAll?: boolean,
+  }) {
+  const { t } = useTranslation()
+  const [tab, setTab] = useAtom(inventoryTabAtom)
+  const [doc] = useAtom(selectedDocTileAtom)
+  const [, setTheoremInventory] = useAtom(inventoryTilesAtoms[InventoryTab.theorem])
+  const [, setTacticInventory] = useAtom(inventoryTilesAtoms[InventoryTab.tactic])
+  const [, setDefinitionInventory] = useAtom(inventoryTilesAtoms[InventoryTab.definition])
+
+  const subTabs = [...new Set(TabContent(tab, levelInfo).map(item => item.category))]
+
+  // TODO: this is some glue since no everything is in jotai yet
+  useEffect(() => {
+    setTheoremInventory(levelInfo?.lemmas ?? [])
+    setTacticInventory(levelInfo?.tactics ?? [])
+    setDefinitionInventory(levelInfo?.definitions ?? [])
+  }, [levelInfo])
+
+  return (
+    <div className="inventory">
+      <InventoryTabBar />
+      <InventorySubTabBar />
+      {levelInfo &&
+        <InventoryList tiles={TabContent(tab, levelInfo)} docType={tab} enableAll={enableAll} />
+      }
+    </div>
+  )
+}
