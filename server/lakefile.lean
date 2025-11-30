983847ebce36a8ff1a51c266649c0abd31bf469b
Jon Eugster <eugster.jon@gmail.com>
2025-11-30 16:16:30 +0100

    bump v4.24.1

--- server/lakefile.lean
+++ server/lakefile.lean
@@ -6,11 +6,11 @@ package GameServer
 -- Using this assumes that each dependency has a tag of the form `v4.X.0`.
 def leanVersion : String := s!"v{Lean.versionString}"
 
-require "leanprover-community" / batteries @ git leanVersion
+require "leanprover-community" / batteries @ git "v4.24.0"
 require "hhu-adam" / i18n @ git leanVersion
 
 -- dev dependency
-require "leanprover-community" / importGraph @ git leanVersion
+require "leanprover-community" / importGraph @ git "v4.24.0"
 
 @[default_target]
 lean_lib GameServer
