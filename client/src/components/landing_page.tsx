e067f58b9ddd4756228adf72b3aa179231f9d3e4
matlorr <matvey.lorkish@outlook.de>
2025-12-19 16:11:54 +0100

    Revert back to using default useTranslation from i19next in landing page to correctly display languages in tiles.

--- client/src/components/landing_page.tsx
+++ client/src/components/landing_page.tsx
@@ -26,7 +26,6 @@ import { navOpenAtom } from '../store/navigation-atoms';
 
 function Tile({gameId, data}: {gameId: string, data: GameTile|undefined}) {
   let { t } = useTranslation()
-  const { t : gT } = useGameTranslation()
   let navigate = useNavigate();
   const routeChange = () =>{
     navigate(gameId);
@@ -38,17 +37,17 @@ function Tile({gameId, data}: {gameId: string, data: GameTile|undefined}) {
 
   return <div className="game" onClick={routeChange}>
     <div className="wrapper">
-      <div className="title">{gT(data.title)}</div>
-      <div className="short-description">{gT(data.short)}
+      <div className="title">{t(data.title, {ns: gameId})}</div>
+      <div className="short-description">{t(data.short, { ns: gameId })}
       </div>
       { data.image ? <img className="image" src={path.join("data", gameId, data.image)} alt="" /> : <div className="image"/> }
-      <div className="long description"><Markdown>{gT(data.long)}</Markdown></div>
+      <div className="long description"><Markdown>{t(data.long, { ns: gameId })}</Markdown></div>
     </div>
     <table className="info">
       <tbody>
       <tr>
         <td title="consider playing these games first.">{t("Prerequisites")}</td>
-        <td><Markdown>{gT(data.prerequisites.join(', '))}</Markdown></td>
+        <td><Markdown>{t(data.prerequisites.join(', '), { ns: gameId })}</Markdown></td>
       </tr>
       <tr>
         <td>{t("Worlds")}</td>
