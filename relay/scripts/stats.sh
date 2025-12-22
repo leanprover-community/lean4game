d4c11c42adb5b28771e0d0650956a7cbdc6e82dc
Matvey Lorkish <49522528+matlorr@users.noreply.github.com>
2025-12-22 14:43:47 +0100

    Fiix/ram usage (#424)

--- relay/scripts/stats.sh
+++ relay/scripts/stats.sh
@@ -6,7 +6,9 @@ python=/usr/bin/python3
 cpu_usage=$CPU_SCRIPT
 # Execute python script
 cpu=$($python $cpu_usage)
-# Calculate memory usage by computing 1 - %free_memory
-mem=$(free | sed '2q;d' | awk '{print 1 - ($4/$2)}')
+# Calculate memory usage by computing (used memory - cached/buffer memory) divided by total memory
+# We can consider cached/buffer memory to be free.
+# As cached/buffer memory can be greater than the currently used memory we also use its absolute value.
+mem=$(free | sed '2q;d' | awk '{print sqrt(($3-$6)^2)/$2}')
 
 printf "CPU, MEM\n%f, %f\n" $cpu $mem
