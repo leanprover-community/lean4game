cc7e45076efa5c4307d54a8785128720fc02ef17
matlorr <matvey.lorkish@outlook.de>
2026-04-10 17:38:01 +0200

    Non-dynamic textbox in typewriter mode

--- client/src/components/infoview/typewriter.tsx
+++ client/src/components/infoview/typewriter.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { MutableRefObject, useEffect, useRef, useState } from "react";
 
 import "../../css/typewriter.css"
 import { useTranslation } from "react-i18next";
@@ -6,7 +6,14 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faWandMagicSparkles } from "@fortawesome/free-solid-svg-icons";
 import { useAtom } from "jotai";
 import { gameInfoAtom } from "../../store/query-atoms";
-import { gameIdAtom, worldIdAtom } from "../../store/location-atoms";
+
+import { DiagnosticSeverity, PublishDiagnosticsParams, DocumentUri } from 'vscode-languageserver-protocol';
+import * as monaco from 'monaco-editor'
+import { levelIdAtom, gameIdAtom, worldIdAtom } from "../../store/location-atoms";
+import { leanMonacoEditorAtom, typewriterContentAtom, interimDiagsAtom, crashedAtom, leanMonacoEditorModelAtom, leanMonacoEditorUriAtom, hasLeanMonacoEditorAtom, lastProofStepErrorCommandAtom, restoreErrorCommandEffect, oneLineEditorAtom, syncTypewriterToEditorEffect, syncEditorPositionEffect } from "../../store/editor-atoms";
+import { deletedChatAtom } from '../../store/chat-atoms'
+import { preferencesAtom } from '../../store/preferences-atoms'
+
 import path from "node:path";
 import { proofAtom } from "../../store/editor-atoms";
 import { ExerciseStatement } from "./ExerciseStatement";
@@ -50,15 +57,101 @@ export function TypewriterCommandLine() {
     // runCommand()
   }
 
+  let inputRef = getInputRef()
+
   // do not display if the proof is completed (with potential warnings still present)
   return <div className="typewriter">
       <form onSubmit={handleSubmit}>
         <div className="typewriter-input-wrapper">
-          <div className="typewriter-input" />
+          <div ref={inputRef} className="typewriter-input" />
         </div>
         <button type="submit" disabled={false /* TODO */} className="btn btn-inverted">
           <FontAwesomeIcon icon={faWandMagicSparkles} />&nbsp;{t("Execute")}
         </button>
       </form>
     </div>
+}
+
+export function getInputRef() {
+  const [typewriter, setTypewriter] = useAtom(typewriterContentAtom)
+  const [oneLineEditor, setOneLineEditor] = useAtom(oneLineEditorAtom)
+  // added mutability so oneLineEditorRef.current can be reassigned
+  const oneLineEditorRef = useRef<monaco.editor.IStandaloneCodeEditor>(null) as MutableRefObject<monaco.editor.IStandaloneCodeEditor | null>
+  const inputRef = useRef<HTMLDivElement>()
+
+  useAtom(syncTypewriterToEditorEffect)
+  useAtom(syncEditorPositionEffect)
+
+  useEffect(() => {
+    console.log("getInputRef useEffect")
+    if (oneLineEditorRef.current) {
+      console.log("oneLineEditorRef.current is")
+      return
+    }
+
+    const editorConfig: monaco.editor.IStandaloneEditorConstructionOptions = {
+      value: typewriter,
+      language: "lean4",
+      quickSuggestions: false,
+      // lightbulb: {
+      //   enabled: true
+      // },
+      unicodeHighlight: {
+        ambiguousCharacters: false,
+      },
+      automaticLayout: true,
+      minimap: {
+        enabled: false
+      },
+      lineNumbers: 'off',
+      tabSize: 2,
+      wordWrap: 'on',
+      glyphMargin: false,
+      folding: false,
+      lineDecorationsWidth: 0,
+      lineNumbersMinChars: 0,
+      'semanticHighlighting.enabled': true,
+      overviewRulerLanes: 0,
+      hideCursorInOverviewRuler: true,
+      padding: {
+        top: 0,
+        bottom: 0,
+      },
+      scrollbar: {
+        verticalScrollbarSize: 3
+      },
+      scrollBeyondLastLine: false,
+      overviewRulerBorder: false,
+      theme: 'vs-code-theme-converted',
+      contextmenu: false
+    };
+
+    const myEditor = monaco.editor.create(inputRef.current!, editorConfig)
+
+    const layoutInput = () => {
+      const lineHeight = myEditor.getOption(monaco.editor.EditorOption.lineHeight)
+      const height = Math.min(myEditor.getContentHeight(), lineHeight + 2, window.innerHeight / 3)
+      inputRef.current!.style.height = `${height}px`
+      console.log(`width of single line editor: ${inputRef.current!.clientWidth}`)
+      myEditor.layout({
+        width: inputRef.current!.clientWidth,
+        height
+      })
+    }
+
+    myEditor.onDidContentSizeChange(layoutInput)
+    layoutInput()
+
+    oneLineEditorRef.current = myEditor
+    setOneLineEditor(myEditor)
+
+    return () => {
+      // abbrevRewriter.dispose()
+      myEditor.dispose()
+      oneLineEditorRef.current = null
+      setOneLineEditor(null)
+    }
+  }, [])
+
+  return inputRef
 }
--- client/src/store/editor-atoms.ts
+++ client/src/store/editor-atoms.ts
@@ -1,11 +1,15 @@
+import { editor } from 'monaco-editor'
+
 import { atom } from "jotai";
-import { LeanMonaco, LeanMonacoOptions } from 'lean4monaco'
+import { atomEffect } from 'jotai-effect';
+import { LeanMonaco, LeanMonacoOptions, LeanMonacoEditor } from 'lean4monaco'
 import { gameIdAtom } from "./location-atoms";
 import { levelProgressAtom, progressAtom } from "./progress-atoms";
 import { Selection } from "./progress-types";
 import { levelInfoAtom } from "./query-atoms";
 import { ProofState } from "../../../infoview/rpc_api";
-import { Diagnostic } from 'vscode-languageserver-types'
+import { Diagnostic, DiagnosticSeverity } from 'vscode-languageserver-types'
+import { preferencesAtom } from './preferences-atoms';
 
 /** Options for the LeanMonaco instance */
 export const leanMonacoOptionsAtom = atom<LeanMonacoOptions>(get => {
@@ -22,6 +26,31 @@ export const leanMonacoOptionsAtom = atom<LeanMonacoOptions>(get => {
     "editor.wordWrap": true,
   }
 }})
+
+export const rpcSessionAtom = atom
+
+export const leanMonacoEditorAtom = atom<editor.IStandaloneCodeEditor>(null as any)
+
+export const leanMonacoEditorModelAtom = atom(
+  (get) => {
+    const editor = get(leanMonacoEditorAtom)
+    return editor?.getModel()
+  }
+)
+
+export const leanMonacoEditorUriAtom = atom(
+  (get) => {
+    const model = get(leanMonacoEditorModelAtom)
+    return model?.uri.toString() ?? ''
+  }
+)
+
+export const hasLeanMonacoEditorAtom = atom(
+  (get) => {
+    const editor = get(leanMonacoEditorAtom)
+    const model = get(leanMonacoEditorModelAtom)
+    return Boolean(editor && model)
+  })
 
 /** The unique leanMonaco instance for the entire application */
 export const leanMonacoAtom = atom<LeanMonaco | null>(null)
@@ -84,8 +113,94 @@ export const typewriterModeAtom = atom(
  */
 export const proofAtom = atom<ProofState>()
 
+export const restoreErrorCommandEffect = atomEffect(
+  (get, set) => {
+    const errorCommand = get(lastProofStepErrorCommandAtom)
+
+    if (errorCommand) {
+      set(typewriterContentAtom, errorCommand)
+    }
+  }
+)
+
+export const lastProofStepHasErrorsAtom = atom(
+  (get) => {
+    const proof = get(proofAtom)
+    if (!proof?.steps.length) {
+      return false
+    }
+
+    let diags = [...proof.steps[proof.steps.length - 1].diags, ...proof.diagnostics]
+
+    return diags.some(
+      (d) => (d.severity == DiagnosticSeverity.Error)
+    )
+  }
+)
+
+export const lastProofStepErrorCommandAtom = atom(
+  (get) => {
+    const proof = get(proofAtom)
+    const hasErrors = get(lastProofStepHasErrorsAtom)
+
+    if (!hasErrors || !proof?.steps) {
+      return null
+    }
+
+    return proof.steps[proof.steps.length - 1].command
+  }
+)
+
 /** TODO: Workaround to capture a crash of the gameserver. */
 export const interimDiagsAtom = atom<Array<Diagnostic>>([])
 
 /** TODO: Workaround to capture a crash of the gameserver. */
 export const crashedAtom = atom<boolean>(false)
+
+export const syncTypewriterToEditorEffect = atomEffect(
+  (get, set) => {
+    const oneLineEditor = get(oneLineEditorAtom)
+    const typewriter = get(typewriterContentAtom)
+    const { isSuggestionsMobileMode } = get(preferencesAtom)
+
+    if (!oneLineEditor) {
+      console.log("oneLineEditor is False")
+      return
+    }
+
+    if (oneLineEditor.getValue() !== typewriter) {
+      console.log(`Set value in oneLineEditor to ${typewriter}`)
+      oneLineEditor.setValue(typewriter)
+      oneLineEditor.setPosition({
+        column: typewriter.length + 1,
+        lineNumber: 1
+      })
+    }
+
+    if (!isSuggestionsMobileMode) {
+      oneLineEditor.focus()
+    }
+  }
+)
+
+export const oneLineEditorAtom = atom<editor.IStandaloneCodeEditor | null>(null)
+
+export const syncEditorPositionEffect = atomEffect(
+  (get, set) => {
+    const oneLineEditor = get(oneLineEditorAtom)
+    const editor = get(leanMonacoEditorAtom)
+    const hasEditor = get(hasLeanMonacoEditorAtom)
+    const { isSuggestionsMobileMode } = get(preferencesAtom)
+
+    if (!oneLineEditor || !hasEditor || !editor) return
+
+    oneLineEditor.setPosition({
+      column: editor.getValue().length + 1,
+      lineNumber: 1
+    })
+
+    if (!isSuggestionsMobileMode) {
+      oneLineEditor.focus()
+    }
+  }
+)
