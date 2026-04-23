57673f50f7432b28c1759c9fba264faba4fa3161
Jon Eugster <eugster.jon@gmail.com>
2026-04-24 00:20:31 +0200

    test: add test for bugfix #499 (#503)

--- server/Test/HintInBranch.lean
+++ server/Test/HintInBranch.lean
@@ -1,8 +1,43 @@
 import GameServer.Commands
 
+/-!
+Test that hints inside a branch are translated.
+-/
+
 Game "Test"
 World "Test"
 Level 1
+
+/--
+info: There are 3 keys for tranlation: [level completed! 🎉, level completed with warnings… 🎭, intermediate goal solved! 🎉]
+-/
+#guard_msgs in
+ListTranslations
+
+Statement (n : Nat) : 0 + n = n := by
+  Template
+    induction n
+    Hint "outside"
+    Branch
+      skip
+      Hint "inside"
+      simp
+    simp
+    Hint "outside again"
+  simp
+
+/--
+info: There are 6 keys for tranlation: [level completed! 🎉,
+ level completed with warnings… 🎭,
+ intermediate goal solved! 🎉,
+ outside,
+ inside,
+ outside again]
+-/
+#guard_msgs in
+ListTranslations
+
+Level 2
 
 Statement : True := by
   Branch
