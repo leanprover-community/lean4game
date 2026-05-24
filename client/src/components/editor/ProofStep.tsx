b7717d2426a1a1e595d748a89ee82f33e0e762c2
matlorr <matvey.lorkish@outlook.de>
2026-05-24 12:20:18 +0200

    Add text box displaying last executed command and show completion message if proof is completed.

--- client/src/components/editor/ProofStep.tsx
+++ client/src/components/editor/ProofStep.tsx
@@ -1,6 +1,26 @@
 import React from "react";
 import { InteractiveGoalsWithHints } from "../../api/rpc_api";
+import { Button } from "../button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDeleteLeft } from "@fortawesome/free-solid-svg-icons";
+import { useAtom } from "jotai";
+import { proofAtom } from "../../store/editor-atoms";
 
 export function ProofStep({step, idx}: {step:InteractiveGoalsWithHints, idx: number}) {
-  return <div key={idx} >Step {idx}</div>
+  const [proofState] = useAtom(proofAtom)
+
+  return <div key={idx} >Step {idx} {step.command}
+    <div className="command">
+        <div className="command-text">{step.command}</div>
+        <Button  className="undo-button btn btn-inverted" title={"Retry proof from here"}>
+          <FontAwesomeIcon icon={faDeleteLeft} />&nbsp;{"Retry"}
+        </Button>
+      </div>
+    <div>
+      {proofState?.completed &&
+        <div className="information ml1 message">
+          <pre className="font-code pre-wrap">level completed! 🎉</pre>
+        </div>}
+    </div>
+  </div>
 }
